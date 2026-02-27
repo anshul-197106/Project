d973eba04f5cfaa60d19017c501e09eeb2ece3e9
Anshul <150144974+anshul-197106@users.noreply.github.com>
2026-02-27 13:29:18 +0530

    Delete backend directory

--- backend/orders/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Order
-
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'gig', 'buyer', 'status', 'amount', 'created_at']
-    list_filter = ['status']
-    search_fields = ['gig__title', 'buyer__username']
--- backend/reviews/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Review
-
-
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ['id', 'gig', 'reviewer', 'rating', 'created_at']
-    list_filter = ['rating']
-    search_fields = ['gig__title', 'reviewer__username', 'comment']
